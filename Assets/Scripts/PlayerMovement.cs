7fac54f605ef89f7aa8160c0be79d4df22fb921c
Masterkedi <yunusefeersoz@hotmail.com>
2023-11-17 18:02:54 +0300

    refactor PlayerMov

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
 
@@ -11,13 +13,13 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 10f;
     private bool isFacingRight = true;
 
-    [SerializeField] private Rigidbody2D rb;
+    private Rigidbody2D rb;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-
+        rb=GetComponent<Rigidbody2D>();
     }
+   
 
     // Update is called once per frame
     void Update()
